3dd46d559a0786e5ef6eda404dd573b2fa0b3ee4
Divyansh Bhardwaj <divyanshb22@gmail.com>
2020-03-24 09:35:11 +0530

    Assignment completed.
    Signed-off-by: Aryan Garg <aryan.garg_cs18@gla.ac.in>

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,19 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.insert("aryan", 1);
+        obj.insert("nikhil", 4);
+        obj.insert("shukl", 3);
+        obj.insert("prateek", 2);
+        obj.insert("ritik", 5);
+        obj.display();
+    }
 }
